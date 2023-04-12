ff1a4943702a0174b2e54166e80427fe4cc49ded
Colin Swaney <colinswaney@princeton.edu>
2023-04-12 11:19:48 -0400

    Add import to hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,10 @@
+import { Configuration, OpenAIApi } from 'openai';
+
 exports.handler = async function (event) {
   console.log("This isn't really a secret: ", process.env.TEST_VARIABLE);
   const response = {
     role: 'user',
-    content: 'Well hello there!',
+    content: process.env.TEST_VARIABLE,
   }
   return {
     statusCode: 200,
