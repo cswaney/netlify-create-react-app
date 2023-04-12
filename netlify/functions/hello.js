4613b2ee8ec392483b3c9e5ddc399026da6780b1
Colin Swaney <colinswaney@princeton.edu>
2023-04-12 11:12:23 -0400

    Add test env variable to hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event) {
-  console.log("Well hello there!");
+  console.log("This isn't really a secret: ", process.env.TEST_VARIABLE);
   const response = {
     role: 'user',
     content: 'Well hello there!',
